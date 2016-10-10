12a56c5bf394752693165e3f05808a8ccf5670f9
Pablo Molnar <pablomolnar@gmail.com>
2016-10-10 14:40:12 -0700

    Update trees.populate_next_right.groovy

--- trees.populate_next_right.groovy
+++ trees.populate_next_right.groovy
@@ -20,6 +20,7 @@ root.right.left.right = new Node(value: 'H')
 
 populate_next_right(root)
 populate_next_right_2(root)
+populate_next_right_3(root)
 
 // With level field
 void populate_next_right(Node root) {
@@ -72,6 +73,34 @@ void populate_next_right_2(Node root) {
 			left_node = null
 		} else {
 			left_node = node
+		}
+	}
+}
+
+void populate_next_right3(Node root) {
+	queue = [root] as Queue
+	
+	while(!queue.isEmpty()) {
+
+		Node left_node = null
+		size = queue.size()
+		node = queue.poll()
+		
+		for(i=0 ; i<size ; i++) {
+			
+			if(node.left) queue << node.left
+			if(node.right)  queue << node.right
+
+			if(left_node) {
+				left_node.nextRight  = node
+				println "$left_node.value --> $node.value"
+			}
+			
+			left_node = node
+			if(i + 1 < size) {
+				node = queue.poll()
+			}
+
 		}
 	}
 }
